6c0f26d92f760e61a8f9cd92ae7272f2e248dca9
JD <Doriac.jd@hotmail.fr>
2020-10-13 01:02:15 +0200

    Prototype + Working on S1
    Fix player moovement and cameras issuess

---
+++ Assets/Guard.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guard : MonoBehaviour {
+
+    public GameObject waypointsHolder;
+
+    List<Transform> waypoints = new List<Transform>();
+
+    public float speed = 10f;
+    public float waitTime = 1f;
+    public LayerMask layertoCheck; // sensé etre la couche du joueur
+    //spot
+    private Light spotLight;
+
+    Color originalSpotColor;
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start() {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        spotLight = transform.GetChild(0).GetComponent<Light>();
+        originalSpotColor = spotLight.color;
+        foreach (Transform item in waypointsHolder.transform) {
+            waypoints.Add(item);
+        }
+
+        if (waypoints != null && waypoints.Count > 0) {
+            StartCoroutine(Patrol(waypoints));
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (ISEEYOU()) {
+            spotLight.color = Color.red;
+        } else {
+            //print("nein");
+            spotLight.color = originalSpotColor;
+        }
+    }
+
+    // return true if the player is in range, is visible and isInAngle
+    bool ISEEYOU() {
+        Vector3 dirToPlyer = (player.transform.position - transform.position).normalized;
+        //print("Range : " + inRange(player.transform.position, spotLight.range));
+        //print("Angle : " + isBetweenAngle(transform.forward, dirToPlyer, spotLight.spotAngle));
+        //print("Visible : " + isVisible(transform.position, player.transform.position));
+        if (inRange(player.transform.position, spotLight.range) && isBetweenAngle(transform.forward, dirToPlyer, spotLight.spotAngle) && isVisible(transform.position, player.transform.position)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    bool isBetweenAngle(Vector3 from, Vector3 to, float angleTocheck) {
+        if (Vector3.Angle(from, to) < angleTocheck) {
+            return true;
+        }
+        return false;
+    }
+
+
+    bool inRange(Vector3 check, float distanceToCheck) {
+        if (Vector3.Distance(transform.position, check) < distanceToCheck) {
+            return true;
+        }
+        return false;
+    }
+
+
+    bool isVisible(Vector3 start, Vector3 end) {
+
+
+        if (!Physics.Linecast(start, end, ~layertoCheck)) {
+
+            return true;
+        }
+        return false;
+    }
+
+    IEnumerator Patrol(List<Transform> waypoints) {
+        transform.position = waypoints[0].position;
+
+        int index = 0;
+
+        while (true) {
+            transform.position = Vector3.MoveTowards(transform.position, waypoints[index].position, speed * Time.deltaTime);
+            if (transform.position == waypoints[index].position) {
+
+                index = (index + 1) % waypoints.Count;
+
+                yield return new WaitForSeconds(waitTime);
+            }
+            yield return null;
+        }
+
+
+    }
+
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.green;
+        if (player) {
+            //Gizmos.DrawLine(transform.position, player.transform.position);
+            drawAnlge(spotLight.spotAngle, spotLight.range);
+        }
+
+        Gizmos.color = Color.black;
+        if (waypoints.Count > 0) {
+            foreach (Transform item in waypoints) {
+                Gizmos.DrawSphere(item.position, .2f);
+            }
+        }
+
+
+
+    }
+
+    void drawAnlge(float fov, float range) {
+        float totalFOV = fov;
+        float rayRange = range;
+        float halfFOV = totalFOV / 2.0f;
+        Quaternion leftRayRotation = Quaternion.AngleAxis(-halfFOV, Vector3.up);
+        Quaternion rightRayRotation = Quaternion.AngleAxis(halfFOV, Vector3.up);
+        Vector3 leftRayDirection = leftRayRotation * transform.forward;
+        Vector3 rightRayDirection = rightRayRotation * transform.forward;
+        Gizmos.DrawRay(transform.position, leftRayDirection * rayRange);
+        Gizmos.DrawRay(transform.position, rightRayDirection * rayRange);
+    }
+}
---
+++ Assets/Player/Scripts/PlayerController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+
+    public Transform follow;
+    public float movespeed = 2f;
+    public float turnSmoothTime = 0.1f;
+
+    float turnSmoothVelocity;
+    CharacterController controller;
+    NavMeshAgent agent;
+    int defaultLayer;
+
+    private static readonly string hiddenLayer = "Dig";
+
+    private void Awake() {
+        controller = GetComponent<CharacterController>();
+        agent = GetComponent<NavMeshAgent>();
+        defaultLayer = gameObject.layer;
+    }
+
+    private void Start() {
+        //agent.enabled = true;
+    }
+
+    private void Update() {
+
+        Moovement();
+
+        if (Input.GetKey(KeyCode.Mouse0) && IsOn(hiddenLayer)) {
+            Debug.Log(" je me cache / animation de creusage tu sais deja");
+            Hidding();
+        } else if (!Input.GetKey(KeyCode.Mouse0) && IsOn(hiddenLayer)) {
+            Debug.Log("JETAIS CACHE");
+            NotHidding();
+        }
+
+
+    }
+
+
+    void Moovement() {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertital = Input.GetAxisRaw("Vertical");
+        Vector3 dir = new Vector3(horizontal, 0f, vertital).normalized;
+
+        if (dir.magnitude >= 0.1f) {
+
+            float targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            controller.Move(dir * movespeed * Time.deltaTime);
+        }
+       
+    }
+
+
+    // Is mouse currently on "area"
+    public bool IsOn(string area) {
+        return getCurrentArea() == (1 << NavMesh.GetAreaFromName(area));
+    }
+
+    //return current mouse Area
+    public int getCurrentArea() {
+        NavMeshHit hit;
+        agent.SamplePathPosition(agent.areaMask, 0.01f, out hit);
+        return hit.mask;
+    }
+
+    void Hidding() {    
+        gameObject.layer = (1 << NavMesh.GetAreaFromName(hiddenLayer));
+    }
+    void NotHidding() {
+        gameObject.layer = defaultLayer;
+    }
+}
+
